b385ccb52bf57919984193b09269b650864e5f1c
Mohammed Rafeeq <mohammed.rafeeq@hmcts.net>
2026-05-29 14:20:48 +0100

    DFR-5034: configured nightly build

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/finrem/taskconfiguration/SmokeTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.finrem.taskconfiguration;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.HttpStatus.OK;
+
+class SmokeTest {
+
+    private String testUrl = System.getenv().getOrDefault("TEST_URL", "http://localhost:4558");
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = testUrl;
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Test
+    void healthCheck() {
+        given()
+            .get("/health")
+            .then()
+            .statusCode(OK.value())
+            .body("status", equalTo("UP"));
+    }
+}
