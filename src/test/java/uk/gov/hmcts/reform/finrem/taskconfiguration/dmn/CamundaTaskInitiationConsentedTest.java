78c701353870da5d960ebf57ac8461628fb8a851
Mohammed Rafeeq <mohammed.rafeeq@hmcts.net>
2026-06-05 16:10:37 +0100

    DFR-5072: added delayUntil in initiation dmn

--- src/test/java/uk/gov/hmcts/reform/finrem/taskconfiguration/dmn/CamundaTaskInitiationConsentedTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/taskconfiguration/dmn/CamundaTaskInitiationConsentedTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class CamundaTaskInitiationConsentedTest extends DmnDecisionTableBaseUnitTest {
@@ -45,14 +46,20 @@ class CamundaTaskInitiationConsentedTest extends DmnDecisionTableBaseUnitTest {
         inputVariables.putValue("postEventState", "");
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
-        assertThat(dmnDecisionTableResult.getResultList(), is(List.of(
-            Map.of(
-                "taskId", "processScannedDocuments",
-                "name", "Process Scanned Documents",
-                "delayDuration", 0,
-                "processCategories", "CHANGE_LATER_PROCESS_CATEGORIES"
-            )
-        )));
+        List<Map<String, Object>> results = dmnDecisionTableResult.getResultList();
+
+        assertThat(results.size(), is(1));
+        Map<String, Object> result = results.get(0);
+        assertThat(result.get("taskId"), is("processScannedDocuments"));
+        assertThat(result.get("name"), is("Process Scanned Documents"));
+        assertThat(result.get("delayDuration"), is(0));
+        assertThat(result.get("processCategories"), is("CHANGE_LATER_PROCESS_CATEGORIES"));
+
+        // delayUntil is a json map; its delayUntil value is now() so it is non-deterministic
+        @SuppressWarnings("unchecked")
+        Map<String, Object> delayUntil = (Map<String, Object>) result.get("delayUntil");
+        assertThat(delayUntil.get("delayUntilIntervalDays"), is("0"));
+        assertThat(delayUntil.get("delayUntil"), is(notNullValue()));
     }
 
     @Test
