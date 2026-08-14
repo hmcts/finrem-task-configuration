6c3f2b1f2b3f9d7587844e6fd77db81c7d3a7358
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-08-14 13:19:21 +0100

    DFR-5549: Add test for FR_close cancellation

--- src/test/java/uk/gov/hmcts/reform/finrem/taskconfiguration/dmn/CamundaTaskCancellationConsentedTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/taskconfiguration/dmn/CamundaTaskCancellationConsentedTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.finrem.taskconfiguration.DmnDecisionTable;
 import uk.gov.hmcts.reform.finrem.taskconfiguration.DmnDecisionTableBaseUnitTest;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CamundaTaskCancellationConsentedTest extends DmnDecisionTableBaseUnitTest {
@@ -27,6 +30,24 @@ class CamundaTaskCancellationConsentedTest extends DmnDecisionTableBaseUnitTest 
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
         assertThat(dmnDecisionTableResult.getResultList()).isEmpty();
+    }
+
+    @Test
+    void givenCloseEventShouldReturnCancel() {
+        VariableMap inputVariables = new VariableMapImpl();
+        inputVariables.putValue("fromState", "");
+        inputVariables.putValue("event", "FR_close");
+        inputVariables.putValue("state", "");
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+        List<Map<String, Object>> results = dmnDecisionTableResult.getResultList();
+
+        assertThat(results).hasSize(1);
+        Map<String, Object> result = results.getFirst();
+        assertThat(result).containsEntry("action", "Cancel");
+        assertThat(result.get("warningCode")).isEqualTo(null);
+        assertThat(result.get("warningText")).isEqualTo(null);
+        assertThat(result.get("processCategories")).isEqualTo(null);
     }
 
     @Test
