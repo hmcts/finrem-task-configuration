9f477a29d9c4adac17870952ccef59f8391fe595
Chris Wheatley <christopher.wheatley@hmcts.net>
2026-09-07 10:37:48 +0100

    Removed Assign to Judge next step for DFR-5626

--- src/test/java/uk/gov/hmcts/reform/finrem/taskconfiguration/dmn/CamundaTaskCompletionConsentedTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/taskconfiguration/dmn/CamundaTaskCompletionConsentedTest.java
@@ -63,7 +63,7 @@ class CamundaTaskCompletionConsentedTest extends DmnDecisionTableBaseUnitTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"FR_referToJudge","FR_issueApplicationFromInfoReceived","FR_awaitingInfo","FR_generalEmail",
+    @ValueSource(strings = {"FR_issueApplicationFromInfoReceived","FR_awaitingInfo","FR_generalEmail",
         "FR_generalLetter","FR_generalOrderFromInfoReceived","FR_listForHearing","FR_close"})
     void givenCheckResponseReceivedEvents_whenEvaluated_thenCompletesTask(String eventId) {
         VariableMap inputVariables = new VariableMapImpl();
